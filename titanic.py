c455e1ff7e2a4ed58bead43fc32b29b0d8461a93
Preston_note <kyzoon@163.com>
2017-06-24 21:35:27 +0800

    titanic2
    Add Columns 'Child' and 'FamilySize'. But score becoming lower, is 0.76555. The last score is 0.77033(#4650)

--- titanic.py
+++ titanic.py
@@ -44,5 +44,46 @@ def titanic1():
 		"Survived": res_survived})
 	res.to_csv("prediction.csv", index=False)
 
+def titanic2():
+	train = pd.read_csv("input/train.csv").copy()
+	test = pd.read_csv("input/test.csv").copy()
+
+	X = train.drop(["Cabin", "Name", "Ticket", "Survived", "PassengerId"], axis=1)
+	X["Sex"] = X["Sex"].replace(["male", "female"], [0, 1])
+	X["Embarked"] = X["Embarked"].replace(["S", "C", "Q"], [0, 1, 2])
+	X["Embarked"] = X["Embarked"].fillna(0)
+	X["Age"] = X["Age"].fillna(X["Age"].mean())
+	X["Child"] = float('NaN')
+	X["Child"][X["Age"] < 18] = 1
+	X["Child"][X["Age"] >= 18] = 0
+	X["FamilySize"] = 1 + X["SibSp"] + X["Parch"]
+	y = train["Survived"]
+
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+	clf = svm.SVC(kernel='linear').fit(X_train, y_train)
+	accuracy = clf.score(X_test, y_test)
+	print "Accuracy: %d" % accuracy
+
+	Xt = test.drop(["Cabin", "Name", "Ticket", "PassengerId"], axis=1)
+	Xt["Sex"] = Xt["Sex"].replace(["male", "female"], [0, 1])
+	Xt["Embarked"] = Xt["Embarked"].replace(["S", "C", "Q"], [0, 1, 2])
+
+	Xt["Embarked"] = Xt["Embarked"].fillna(0)
+	Xt["Age"] = Xt["Age"].fillna(X["Age"].mean())
+	Xt["Fare"] = Xt["Fare"].fillna(X["Fare"].mean())
+
+	Xt["Child"] = float('NaN')
+	Xt["Child"][Xt["Age"] < 18] = 1
+	Xt["Child"][Xt["Age"] >= 18] = 0
+	Xt["FamilySize"] = 1 + Xt["SibSp"] + Xt["Parch"]
+	res_survived = clf.predict(Xt)
+
+	res = pd.DataFrame({
+		"PassengerId": test["PassengerId"],
+		"Survived": res_survived})
+	res.to_csv("prediction2.csv", index=False)
+
 if __name__ == '__main__':
-	titanic1()
+	# titanic1()
+	titanic2()
